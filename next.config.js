ebe2725d0831e7c185c318af2eb9c16089dd1db9
Ricardo Costa <ricardo.costa@corum-am.com>
2022-04-08 11:43:12 +0100

    remove not needed headers

--- next.config.js
+++ next.config.js
@@ -6,18 +6,5 @@ module.exports = {
     webpack5: true
   },
   swcMinify: true,
-  basePath: "",
-  async headers() {
-    return [
-      {
-        source: "/fonts/VT323.ttf",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable"
-          }
-        ]
-      }
-    ]
-  }
+  basePath: ""
 }
