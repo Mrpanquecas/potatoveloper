557bd2251d58477db95180179fa7d54f0e6b7b2b
Ricardo Costa <ricardo.costa@corum-am.com>
2022-04-19 10:04:48 +0100

    remove unused code

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,8 +1,5 @@
 import "@testing-library/jest-dom/extend-expect"
 import { render, screen } from "@testing-library/react"
-import { renderHook } from '@testing-library/react-hooks';
-import { useClippy, AGENTS, ClippyProvider } from '@react95/clippy';
-import HomeScreen from "main/HomeScreen"
 import Main from "../pages/_app"
 
 describe("it renders the homescreen", () => {
@@ -13,17 +10,4 @@ describe("it renders the homescreen", () => {
     expect(screen.getByTestId("slide3")).toBeInTheDocument()
     expect(screen.getByTestId("slide4")).toBeInTheDocument()
   })
-})
-
-
-describe("it renders clippy", () => {
-  it('should get agent from ClippyContext', () => {
-    const wrapper = ({ children }) => (
-      <ClippyProvider>{children}</ClippyProvider>
-    );
-    const { result } = renderHook(() => useClippy(), { wrapper });
-    const { clippy: agent } = result.current;
-
-    expect(agent).toBeTruthy();
-  });
 })
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import "../styles/global.css"
 import original from "react95/dist/themes/original"
 import { createContext } from "react"
 import { ThemeProvider } from "styled-components"
-import { ClippyProvider } from "@react95/clippy"
 
 const Context = createContext()
 
@@ -11,9 +10,7 @@ const Main = ({Component, pageProps}) => {
   return (
     <Context.Provider>
       <ThemeProvider theme={original}>
-        <ClippyProvider>
-          <Component {...pageProps} />
-        </ClippyProvider>
+        <Component {...pageProps} />
       </ThemeProvider>
     </Context.Provider>
   )
