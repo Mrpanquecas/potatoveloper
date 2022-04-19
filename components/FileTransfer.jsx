85599dc3d532145b66913e9abf89e12ef4e0e09a
Ricardo Costa <ricardo.costa@corum-am.com>
2022-04-19 10:41:43 +0100

    change file transfer icon behaviour

--- components/FileTransfer.jsx
+++ components/FileTransfer.jsx
@@ -3,10 +3,10 @@ import { motion } from "framer-motion"
 
 const calculateMargin = (pageProgress) => {
   if (pageProgress < 15) {
-    return 0
+    return 2
   }
   if (pageProgress > 90) {
-    return (pageProgress - 10) * 3.4
+    return (pageProgress - 10) * 3.35
   }
   return pageProgress * 3
 }
@@ -15,21 +15,23 @@ const FileTransfer = ({ pageProgress }) => (
     <div className="absolute flex justify-center w-full z-10">
       <Image width="180" height="180" src={`/images/folder.png`} alt="folder" />
     </div>
-    <motion.div
-      className="absolute flex justify-center w-full z-0 rotate-12"
-      animate={{
-        top: `${calculateMargin(pageProgress)}%`
-      }}
-      transition={{ type: "tween", duration: 0.5 }}
-      initial={false}
-    >
-      <Image
-        width="100"
-        height="100"
-        src={`/images/file.png`}
-        alt="folderEmpty"
-      />
-    </motion.div>
+    {pageProgress > 0 && (
+      <motion.div
+        className="absolute flex justify-center w-full z-0 rotate-12"
+        animate={{
+          top: `${calculateMargin(pageProgress)}%`
+        }}
+        transition={{ type: "tween", duration: 0.5 }}
+        initial={false}
+      >
+        <Image
+          width="100"
+          height="100"
+          src={`/images/file.png`}
+          alt="folderEmpty"
+        />
+      </motion.div>
+    )}
     <div className="absolute flex justify-center w-full top-[300%] z-10">
       <Image width="180" height="180" src={`/images/folder.png`} alt="folder" />
     </div>
