738ed498345e3a2510f54e83d945baac40bccb3a
Ricardo Costa <ricardo.costa@corum-am.com>
2022-04-19 11:33:01 +0100

    fix image aspect ratio

--- src/main/StartupPage/StartupPage.tsx
+++ src/main/StartupPage/StartupPage.tsx
@@ -7,7 +7,7 @@ const HomeScreen = () => {
     <>
       <div data-testid="ceo" className="newCEO">
         <Desktop backgroundStyles={{ background: "blue" }}>
-          <Image width={155} height={115} src={"/images/newCEO.jpg"} alt="CEO" />
+          <Image priority layout="fill" src={"/images/newCEO.jpg"} alt="CEO" />
         </Desktop>
       </div>
       <span className="expertText">EXPERT IN</span>
