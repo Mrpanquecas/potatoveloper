badd970e0b5b34b100b4fc6c835fe3dd6fcbc810
Ricardo Costa <ricardo.costa@corum-am.com>
2022-04-19 10:48:09 +0100

    add meta description

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
           <link
             href="/fonts/VT323.ttf"
             rel="preload"
