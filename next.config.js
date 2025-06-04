fba3b66a25aa63992e7edb2b35f29c00eae6797c
Ricardo Simões Costa <ricardo@Ricardos-MacBook-Pro.local>
2025-06-04 23:07:55 +0100

    feat: keep console logs in prod

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production"
-  },
-  transpilePackages: ["react95"]
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
